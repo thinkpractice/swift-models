ecd7de82dd143ece1bae43573c3ffc8e2df744b7
Tim De Jong <gitlab@thinkpractice.nl>
2025-12-16 16:51:04 +0100

    Fixed some issues.

--- Package.swift
+++ Package.swift
@@ -32,64 +32,169 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "Checkpoints", dependencies: ["SwiftProtobuf", "ModelSupport"],
+            name: "Checkpoints",
+            dependencies: [
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "ModelSupport"
+            ],
             path: "Checkpoints"),
-        .target(name: "Datasets", dependencies: ["ModelSupport"], path: "Datasets"),
+        .target(
+            name: "Datasets",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "ModelSupport"
+            ],
+            path: "Datasets"),
         .target(name: "STBImage", path: "Support/STBImage"),
         .target(
-            name: "ModelSupport", dependencies: ["STBImage"], path: "Support", exclude: ["STBImage"]
+            name: "ModelSupport",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "STBImage"
+            ],
+            path: "Support",
+            exclude: ["STBImage"]
         ),
         .target(
-            name: "TensorBoard", dependencies: ["SwiftProtobuf", "ModelSupport", "TrainingLoop"],
+            name: "TensorBoard",
+            dependencies: [
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "ModelSupport",
+                "TrainingLoop"
+            ],
             path: "TensorBoard"),
-        .target(name: "ImageClassificationModels", path: "Models/ImageClassification"),
-        .target(name: "VideoClassificationModels", path: "Models/Spatiotemporal"),
+        .target(
+            name: "ImageClassificationModels",
+            dependencies: [.product(name: "TaylorTorch", package: "taylortorch")],
+            path: "Models/ImageClassification"),
+        .target(
+            name: "VideoClassificationModels",
+            dependencies: [.product(name: "TaylorTorch", package: "taylortorch")],
+            path: "Models/Spatiotemporal"),
         .target(
             name: "TextModels",
-            dependencies: ["Checkpoints", "Datasets", "SwiftProtobuf"],
+            dependencies: [
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "Checkpoints",
+                "Datasets"
+            ],
             path: "Models/Text"),
-        .target(name: "RecommendationModels", path: "Models/Recommendation"),
-        .target(name: "TrainingLoop", dependencies: ["ModelSupport"], path: "TrainingLoop"),
+        .target(
+            name: "RecommendationModels",
+            dependencies: [.product(name: "TaylorTorch", package: "taylortorch")],
+            path: "Models/Recommendation"),
+        .target(
+            name: "TrainingLoop",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "ModelSupport"
+            ],
+            path: "TrainingLoop"),
         .executableTarget(
             name: "Autoencoder1D",
-            dependencies: ["Datasets", "ModelSupport", "TrainingLoop", "AutoencoderCallback"],
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "Datasets",
+                "ModelSupport",
+                "TrainingLoop",
+                "AutoencoderCallback"
+            ],
             path: "Autoencoder/Autoencoder1D"),
         .executableTarget(
-            name: "Autoencoder2D", dependencies: ["Datasets", "ModelSupport"],
+            name: "Autoencoder2D",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "Datasets",
+                "ModelSupport"
+            ],
             path: "Autoencoder/Autoencoder2D"),
         .executableTarget(
-            name: "VariationalAutoencoder1D", dependencies: ["Datasets", "ModelSupport"],
+            name: "VariationalAutoencoder1D",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "Datasets",
+                "ModelSupport"
+            ],
             path: "Autoencoder/VAE1D"),
         .target(
-            name: "AutoencoderCallback", dependencies: ["ModelSupport", "TrainingLoop"],
+            name: "AutoencoderCallback",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "ModelSupport",
+                "TrainingLoop"
+            ],
             path: "Autoencoder/Callback"),
-        .executableTarget(name: "Catch", path: "Catch"),
-        .executableTarget(name: "Gym-FrozenLake", path: "Gym/FrozenLake"),
-        .executableTarget(name: "Gym-CartPole", path: "Gym/CartPole"),
-        .executableTarget(name: "Gym-Blackjack", path: "Gym/Blackjack"),
-        .executableTarget(name: "Gym-DQN", path: "Gym/DQN"),
-        .executableTarget(name: "Gym-PPO", path: "Gym/PPO"),
+        .executableTarget(
+            name: "Catch",
+            dependencies: [.product(name: "TaylorTorch", package: "taylortorch")],
+            path: "Catch"),
+        .executableTarget(
+            name: "Gym-FrozenLake",
+            dependencies: [.product(name: "TaylorTorch", package: "taylortorch")],
+            path: "Gym/FrozenLake"),
+        .executableTarget(
+            name: "Gym-CartPole",
+            dependencies: [.product(name: "TaylorTorch", package: "taylortorch")],
+            path: "Gym/CartPole"),
+        .executableTarget(
+            name: "Gym-Blackjack",
+            dependencies: [.product(name: "TaylorTorch", package: "taylortorch")],
+            path: "Gym/Blackjack"),
+        .executableTarget(
+            name: "Gym-DQN",
+            dependencies: [.product(name: "TaylorTorch", package: "taylortorch")],
+            path: "Gym/DQN"),
+        .executableTarget(
+            name: "Gym-PPO",
+            dependencies: [.product(name: "TaylorTorch", package: "taylortorch")],
+            path: "Gym/PPO"),
         .executableTarget(
             name: "VGG-Imagewoof",
-            dependencies: ["Datasets", "ImageClassificationModels", "TrainingLoop"],
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "Datasets",
+                "ImageClassificationModels",
+                "TrainingLoop"
+            ],
             path: "Examples/VGG-Imagewoof"),
         .executableTarget(
-            name: "Regression-BostonHousing", dependencies: ["Datasets"],
+            name: "Regression-BostonHousing",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "Datasets"
+            ],
             path: "Examples/Regression-BostonHousing"),
         .executableTarget(
-            name: "Custom-CIFAR10", dependencies: ["Datasets"],
+            name: "Custom-CIFAR10",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "Datasets"
+            ],
             path: "Examples/Custom-CIFAR10"),
         .executableTarget(
             name: "ResNet-CIFAR10",
-            dependencies: ["Datasets", "ImageClassificationModels", "TrainingLoop"],
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "Datasets",
+                "ImageClassificationModels",
+                "TrainingLoop"
+            ],
             path: "Examples/ResNet-CIFAR10"),
         .executableTarget(
             name: "BigTransfer-CIFAR100",
-            dependencies: ["Datasets", "ImageClassificationModels"],
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "Datasets",
+                "ImageClassificationModels"
+            ],
             path: "Examples/BigTransfer-CIFAR100"),
         .executableTarget(
             name: "Shallow-Water-PDE",
             dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "Benchmark", package: "swift-benchmark"),
                 "ModelSupport"
@@ -97,60 +202,169 @@ let package = Package(
             path: "Examples/Shallow-Water-PDE"),
         .executableTarget(
             name: "LeNet-MNIST",
-            dependencies: ["Datasets", "ImageClassificationModels", "TrainingLoop"],
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "Datasets",
+                "ImageClassificationModels",
+                "TrainingLoop"
+            ],
             path: "Examples/LeNet-MNIST"),
         .executableTarget(
             name: "MobileNetV1-Imagenette",
-            dependencies: ["Datasets", "ImageClassificationModels", "TrainingLoop"],
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "Datasets",
+                "ImageClassificationModels",
+                "TrainingLoop"
+            ],
             path: "Examples/MobileNetV1-Imagenette"),
         .executableTarget(
             name: "MobileNetV2-Imagenette",
-            dependencies: ["Datasets", "ImageClassificationModels", "TrainingLoop"],
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "Datasets",
+                "ImageClassificationModels",
+                "TrainingLoop"
+            ],
             path: "Examples/MobileNetV2-Imagenette"),
         .executableTarget(
             name: "ResNet50-ImageNet",
-            dependencies: ["Datasets", "ImageClassificationModels", "TrainingLoop", "TensorBoard"],
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "Datasets",
+                "ImageClassificationModels",
+                "TrainingLoop",
+                "TensorBoard"
+            ],
             path: "Examples/ResNet50-ImageNet"),
         .executableTarget(
             name: "PersonLab",
             dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
                 "Checkpoints", "ModelSupport",
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ],
             path: "PersonLab"),
         .target(
-            name: "MiniGo", dependencies: ["Checkpoints"], path: "MiniGo", exclude: ["main.swift"]),
+            name: "MiniGo",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "Checkpoints"
+            ],
+            path: "MiniGo",
+            exclude: ["main.swift"]),
         .executableTarget(
-            name: "MiniGoDemo", dependencies: ["MiniGo"], path: "MiniGo", sources: ["main.swift"]),
+            name: "MiniGoDemo",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "MiniGo"
+            ],
+            path: "MiniGo",
+            sources: ["main.swift"]),
         .executableTarget(
-            name: "NeuMF-MovieLens", dependencies: ["RecommendationModels", "Datasets"],
+            name: "NeuMF-MovieLens",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "RecommendationModels",
+                "Datasets"
+            ],
             path: "Examples/NeuMF-MovieLens"),
-        .testTarget(name: "MiniGoTests", dependencies: ["MiniGo"]),
-        .testTarget(name: "ImageClassificationTests", dependencies: ["ImageClassificationModels"]),
-        .testTarget(name: "VideoClassificationTests", dependencies: ["VideoClassificationModels"]),
-        .testTarget(name: "RecommendationModelTests", dependencies: ["RecommendationModels"]),
-        .testTarget(name: "DatasetsTests", dependencies: ["Datasets", "TextModels"]),
+        .testTarget(
+            name: "MiniGoTests",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "MiniGo"
+            ]),
+        .testTarget(
+            name: "ImageClassificationTests",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "ImageClassificationModels"
+            ]),
+        .testTarget(
+            name: "VideoClassificationTests",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "VideoClassificationModels"
+            ]),
+        .testTarget(
+            name: "RecommendationModelTests",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "RecommendationModels"
+            ]),
+        .testTarget(
+            name: "DatasetsTests",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "Datasets",
+                "TextModels"
+            ]),
         .executableTarget(
-            name: "GPT2-Inference", dependencies: ["TextModels"],
+            name: "GPT2-Inference",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "TextModels"
+            ],
             path: "Examples/GPT2-Inference",
             exclude: ["UI/Windows/main.swift", "UI/macOS/main.swift"]),
         .executableTarget(
             name: "GPT2-WikiText2",
-            dependencies: ["Datasets", "TextModels", "TrainingLoop", "TensorBoard"],
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "Datasets",
+                "TextModels",
+                "TrainingLoop",
+                "TensorBoard"
+            ],
             path: "Examples/GPT2-WikiText2"),
-        .testTarget(name: "TextTests", dependencies: ["TextModels"]),
-        .executableTarget(name: "GAN", dependencies: ["Datasets", "ModelSupport"], path: "GAN"),
-        .executableTarget(name: "DCGAN", dependencies: ["Datasets", "ModelSupport"], path: "DCGAN"),
+        .testTarget(
+            name: "TextTests",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "TextModels"
+            ]),
+        .executableTarget(
+            name: "GAN",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "Datasets",
+                "ModelSupport"
+            ],
+            path: "GAN"),
+        .executableTarget(
+            name: "DCGAN",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "Datasets",
+                "ModelSupport"
+            ],
+            path: "DCGAN"),
         .target(
-            name: "FastStyleTransfer", dependencies: ["Checkpoints"], path: "FastStyleTransfer",
+            name: "FastStyleTransfer",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "Checkpoints"
+            ],
+            path: "FastStyleTransfer",
             exclude: ["Demo"]),
         .executableTarget(
-            name: "FastStyleTransferDemo", dependencies: ["FastStyleTransfer"],
+            name: "FastStyleTransferDemo",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "FastStyleTransfer"
+            ],
             path: "FastStyleTransfer/Demo"),
-        .testTarget(name: "FastStyleTransferTests", dependencies: ["FastStyleTransfer"]),
+        .testTarget(
+            name: "FastStyleTransferTests",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "FastStyleTransfer"
+            ]),
         .target(
             name: "SwiftModelsBenchmarksCore",
             dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
                 "Datasets", "ModelSupport", "ImageClassificationModels",
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 "TextModels",
@@ -159,19 +373,39 @@ let package = Package(
             path: "SwiftModelsBenchmarksCore"),
         .executableTarget(
             name: "SwiftModelsBenchmarks",
-            dependencies: ["SwiftModelsBenchmarksCore"],
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "SwiftModelsBenchmarksCore"
+            ],
             path: "SwiftModelsBenchmarks"
         ),
         .testTarget(
-            name: "CheckpointTests", dependencies: ["Checkpoints", "ImageClassificationModels"]),
+            name: "CheckpointTests",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "Checkpoints",
+                "ImageClassificationModels"
+            ]),
         .executableTarget(
-            name: "BERT-CoLA", dependencies: ["TextModels", "Datasets", "TrainingLoop"],
+            name: "BERT-CoLA",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "TextModels",
+                "Datasets",
+                "TrainingLoop"
+            ],
             path: "Examples/BERT-CoLA"),
-        .testTarget(name: "SupportTests", dependencies: ["ModelSupport"]),
+        .testTarget(
+            name: "SupportTests",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "ModelSupport"
+            ]),
         .executableTarget(
             name: "CycleGAN",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "TaylorTorch", package: "taylortorch"),
                 "ModelSupport",
                 "Datasets"
             ],
@@ -181,6 +415,7 @@ let package = Package(
             name: "pix2pix",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "TaylorTorch", package: "taylortorch"),
                 "ModelSupport",
                 "Datasets",
                 "Checkpoints"
@@ -189,12 +424,19 @@ let package = Package(
             exclude: ["main.swift"]
         ),
         .executableTarget(
-            name: "pix2pixDemo", dependencies: ["pix2pix"], path: "pix2pix", sources: ["main.swift"]
+            name: "pix2pixDemo",
+            dependencies: [
+                .product(name: "TaylorTorch", package: "taylortorch"),
+                "pix2pix"
+            ],
+            path: "pix2pix",
+            sources: ["main.swift"]
         ),
         .executableTarget(
             name: "WordSeg",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "TaylorTorch", package: "taylortorch"),
                 "Datasets",
                 "ModelSupport",
                 "TextModels"
